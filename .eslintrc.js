4bb890c670689d382265c61392a8221202b2293e
lingjianghua <lingjianghua@cvte.com>
2018-09-28 20:41:10 +0800

    [modify]添加readme

---
+++ .eslintrc.js
@@ -0,0 +1,117 @@
+const fs = require('fs');
+
+const prettierOptions = JSON.parse(fs.readFileSync('./.prettierrc', 'utf8'));
+
+module.exports = {
+  parser: 'babel-eslint',
+  extends: ['airbnb'],
+  plugins: ['react', 'jsx-a11y'],
+  env: {
+    browser: true,
+    node: true,
+    jest: true,
+    es6: true,
+  },
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  rules: {
+    'arrow-body-style': [2, 'as-needed'],
+    'class-methods-use-this': 0,
+    'prefer-rest-params': 0,
+    'comma-dangle': [2, 'always-multiline'],
+    'dot-notation': 0,
+    camelcase: 0,
+    'import/imports-first': 0,
+    'import/newline-after-import': 0,
+    'import/no-dynamic-require': 0,
+    'import/no-extraneous-dependencies': 0,
+    'import/no-named-as-default': 0,
+    'import/no-unresolved': 2,
+    'import/no-webpack-loader-syntax': 0,
+    'import/prefer-default-export': 0,
+    'import/first': 0,
+    'function-paren-newline': 0,
+    indent: [
+      2,
+      2,
+      {
+        SwitchCase: 1,
+      },
+    ],
+    'max-len': 0,
+    'newline-per-chained-call': 0,
+    'no-confusing-arrow': 0,
+    'no-console': 1,
+    'no-use-before-define': 0,
+    'no-useless-constructor': 0,
+    'one-var': 0,
+    'one-var-declaration-per-line': 0,
+    'object-shorthand': 0,
+    'object-curly-newline': 0,
+    'operator-assignment': 0,
+    'prefer-template': 2,
+    'global-require': 0,
+    radix: 0,
+    'require-yield': 0,
+    'space-before-function-paren': 0,
+    'spaced-comment': 0,
+    'linebreak-style': 0,
+    'func-names': 0,
+    'consistent-return': 0,
+    'no-console': 0,
+    'no-bitwise': 0,
+    'no-plusplus': 0,
+    'no-param-reassign': 0,
+    'no-underscore-dangle': 0,
+    'no-else-return': 0,
+    'no-useless-escape': 0,
+    'no-nested-ternary': 0,
+    'quote-props': 0,
+    'prefer-destructuring': 0,
+    'no-unused-expressions': 0,
+    'no-restricted-syntax': 0,
+    'no-loop-func': 0,
+    'no-mixed-operators': [
+      'error',
+      {
+        groups: [
+          // ["+", "-", "*", "/", "%", "**"],
+          ['&', '|', '^', '~', '<<', '>>', '>>>'],
+          ['==', '!=', '===', '!==', '>', '>=', '<', '<='],
+          ['&&', '||'],
+          ['in', 'instanceof'],
+        ],
+        allowSamePrecedence: true,
+      },
+    ],
+    'arrow-parens': 0,
+    'array-callback-return': 0,
+    'arrow-body-style': 0,
+    'prefer-template': 0,
+    'wrap-iife': 0,
+    'no-unused-vars': [
+      2,
+      {
+        // 允许声明未使用变量
+        vars: 'local',
+        // 参数不检查
+        args: 'none',
+      },
+    ],
+    'no-return-assign': 0,
+    'template-curly-spacing': 'off',
+    indent: 'off',
+  },
+  settings: {
+    'import/resolver': {
+      webpack: {
+        config: './internals/webpack/webpack.prod.babel.js',
+      },
+    },
+  },
+};
--- README.md
+++ README.md
@@ -1,2 +1,118 @@
 # puppeteer-screenshot
+
 A screenshot tool with puppeteer
+
+## 使用 puppeteer 的截图服务
+
+主要希望能传入一段 HTML，或者一个 URL，然后能输出一张图片。
+
+### get start
+
+```
+npm install puppeteer-screenshot
+```
+
+或者
+
+```
+yarn add puppeteer-screenshot
+```
+
+### usage
+
+```
+const Screenshot = require('puppeteer-screenshot');
+
+const screenshot = new Screenshot({
+  optimize: true,
+  storage: {
+    type: 'filesystem',
+    path: ''
+  },
+  timeout: 30000,
+  view: {
+    deviceScaleFactor: 2, // 可以理解为多倍图
+    width: 750,  
+    height: 1200  
+  },
+  captureOption: {
+    type: 'jpeg',
+    quality: 75,
+    fullPage: false, // 是否截取全屏
+    clip: { // 裁剪
+      x: 0,
+      y: 0,
+      width: 400,
+      height: 400,
+      omitBackground: false // 是否隐藏背景
+    }
+  },
+  hooks: {
+    beforeCapture: function(){
+      // do something
+    },
+    afterCapture: function(){
+      // do something
+    }
+  }  
+})
+
+screenshot.capture({
+  type: 'url',
+  url: 'http://www.baidu.com'
+})
+```
+
+### Options
+
+```
+const screenshot = new Screenshot(options);
+```
+
+| 参数名        | 类型             | 必填 | 描述         |
+| ------------- | ---------------- | ---- | ------------ |
+| optimize      | boolean(number)  | 否   | 是否压缩图片 |
+| timeout       | number           | 否   | 超时时间     |
+| storage       | object(function) | 否   | 存储方式     |
+| view          | object           | 否   | 窗口大小     |
+| captureOption | object           | 否   | 截图参数     |
+| hooks         | object           | 否   | 钩子         |
+
+##### optimize 是否压缩图片
+
+* [ boolean ]: false, 默认不开启图片压缩
+* [ number ]: 1-100, 图片质量，数值越小，质量越差
+
+#### timeout 超时时间
+
+* [number]: 30000, 默认是 30000， 这个超时时间是指加载页面的超时时间
+
+#### storage 存储方式
+
+* `type`: 目前支持两种：`filesystem`，`qiniu`。 `filesystem`即存储截图到本地文件系统， `qiniu`是存储截图到[七牛](https://developer.qiniu.com/)服务;
+* `path`: 本地文件系统路径，默认是`process.cwd()`，仅在`type: "filesystem"`有效;
+* `config`: 七牛参数，具体参考：https://developer.qiniu.com/kodo/sdk/1289/nodejs
+
+我们也可以自定义存储方式，传值`function`时，可以自定义存储
+
+#### view 窗口大小
+
+* `deviceScaleFactor`: number, 窗口缩放，默认是 1 ，当数值为 2 可以理解为截图是两倍图；
+* `width`: number, 单位`px`， 默认 1280
+* `height`: number, 单位`px`, 默认 720
+
+#### captureOption 截图参数
+
+* `type`: 截图格式，目前只支持`jpeg`和`png`
+* `quality`: 截图质量
+* `fullPage`： boolean 是否截取整页，`false`是只截取可见部分
+* `clip`: 裁剪，具体如下：
+  ```
+  {
+    x: 0,
+    y: 0,
+    width: 400,
+    height: 400,
+    omitBackground: false // 是否隐藏背景
+  }
+  ```
