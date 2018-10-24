063f185016752bf51d6698434de041f96cec107d
lingjianghua <lingjianghua@cvte.com>
2018-10-24 15:05:27 +0800

    [modify]添加七牛上传支持

--- src/middlewares/storage.js
+++ src/middlewares/storage.js
@@ -1,14 +1,20 @@
-import { storageImageToPath } from '../utils';
+import { storageImageToPath, storageImageToQiuniu } from '../utils';
 
 export default async (ctx, next) => {
   console.log('middleware: storage');
   const { options, result, reject } = ctx;
   const { captureOption, storage } = options;
 
+  const buffer =
+    captureOption.encoding === 'base64'
+      ? result.data
+      : Buffer.from(result.data, 'base64');
+
   try {
     if (storage.type === 'filesystem') {
-      await storageImageToPath(result.data, storage, captureOption);
+      await storageImageToPath(buffer, storage, captureOption);
     } else if (storage.type === 'qiniu') {
+      await storageImageToQiuniu(buffer, storage, captureOption);
     }
   } catch (e) {
     reject(e);
--- src/utils.js
+++ src/utils.js
@@ -1,8 +1,8 @@
 import path from 'path';
 import fs from 'fs-extra';
 import uuid from 'uuid/v4';
+import qiniu from 'qiniu';
 
-// const existsAsync = Promise.promisify(fs.)
 /**
  * [isHttp isHttp]
  * 判断字符串是否是http
@@ -16,18 +16,96 @@ export function isHttp(urlString) {
   return httpReg.test(urlString);
 }
 
-export async function storageImageToPath(data, storage, options) {
-  await fs.ensureDir(storage.path);
-  const buffer =
-    options.encoding === 'base64' ? data : Buffer.from(data, 'base64');
-
+export function generateFileName(storage, options) {
   let fileNameWithExtension;
 
-  const fileName = `${storage.prefix}${uuid()}.${fileNameWithExtension}`;
+  const fileName = `${storage.prefix}${uuid()}`;
   if (options.type === 'jpeg') {
     fileNameWithExtension = `${fileName}.jpg`;
   } else {
     fileNameWithExtension = `${fileName}.png`;
   }
-  await fs.outputFile(path.join(storage.path, fileNameWithExtension), buffer);
+
+  return fileNameWithExtension;
+}
+
+export async function storageImageToPath(data, storage, options) {
+  await fs.ensureDir(storage.path);
+
+  const fileNameWithExtension = generateFileName(storage, options);
+
+  const filePath = path.join(storage.path, fileNameWithExtension);
+  await fs.outputFile(filePath, data);
+
+  return {
+    name: fileNameWithExtension,
+    path: filePath,
+  };
+}
+
+const Duplex = require('stream').Duplex;
+export function bufferToStream(buffer) {
+  let stream = new Duplex();
+  stream.push(buffer);
+  stream.push(null);
+  return stream;
+}
+
+export function storageImageToQiuniu(data, storage, options) {
+  const fileNameWithExtension = generateFileName(storage, options);
+
+  const mac = new qiniu.auth.digest.Mac(storage.accessKey, storage.secretKey);
+  const qiniuOptions = {
+    scope: storage.bucket,
+  };
+  const putPolicy = new qiniu.rs.PutPolicy(qiniuOptions);
+  const uploadToken = putPolicy.uploadToken(mac);
+
+  const formUploader = new qiniu.form_up.FormUploader();
+  const putExtra = new qiniu.form_up.PutExtra();
+  const readableStream = bufferToStream(data);
+
+  const defer = (function() {
+    let resolve, reject;
+    const promise = new Promise((res, rej) => {
+      resolve = res;
+      reject = rej;
+    });
+    return { resolve, reject, promise };
+  })();
+
+  formUploader.putStream(
+    uploadToken,
+    fileNameWithExtension,
+    readableStream,
+    putExtra,
+    function(respErr, respBody, respInfo) {
+      if (respErr) {
+        throw respErr;
+      }
+      if (respInfo.statusCode == 200) {
+        defer.resolve({
+          ...respBody,
+          url: getQiniuUrl(respBody.key, storage),
+        });
+      } else {
+        defer.reject(respBody);
+      }
+    },
+  );
+
+  return defer.promise;
+}
+
+export function getQiniuUrl(key, storage) {
+  const mac = new qiniu.auth.digest.Mac(storage.accessKey, storage.secretKey);
+  const config = new qiniu.conf.Config();
+  const bucketManager = new qiniu.rs.BucketManager(mac, config);
+  const bucketDomain = storage.domain;
+  if (storage.bucketType === 'private') {
+    const deadline = parseInt(Date.now() / 1000) + storage.deadline;
+    return bucketManager.privateDownloadUrl(bucketDomain, key, deadline);
+  } else {
+    return bucketManager.publicDownloadUrl(bucketDomain, key);
+  }
 }
